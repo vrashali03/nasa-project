017bf09a331d50e763d893178964de8578d151c0
Vrashali Naitam <vrushali2214@gmail.com>
2023-08-13 17:19:46 +0530

    Initial Commit

---
+++ server/src/routes/planets/planets.router.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const planetRouter = express.Router();
+
+const { httpGetAllPlanets } = require("./planets.controller");
+
+planetRouter.get("/", httpGetAllPlanets);
+
+module.exports = {
+  planetRouter,
+};
